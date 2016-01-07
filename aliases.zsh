d6b1397f93c72b43f9b46aa59e72f4183fa06bcb
pour-toi <admin@pour-toi.info>
2016-01-07 09:20:48 +0100

    Removing ELK aliases, a zsh plugin is better suited for the task

--- aliases.zsh
+++ aliases.zsh
@@ -12,12 +12,3 @@ alias v="vim"
 
 # Git
 alias gits="git status"
-
-# ELK
-alias stelk="sudo systemctl status elasticsearch.service; sudo systemctl status logstash; sudo systemctl status kibana.service;"
-alias stelh="curl 'localhost:9200/_cluster/health?pretty'"
-alias stelno="curl 'localhost:9200/_cat/nodes?v'"
-alias stelproc="curl 'localhost:9200/_nodes/process?pretty'"
-alias stelsh="curl -XGET http://localhost:9200/_cat/shards"
-# open FDs
-alias sysfs="sysctl fs.file-nr"
